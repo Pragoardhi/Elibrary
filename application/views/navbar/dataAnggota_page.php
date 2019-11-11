b08a684dabfd9cf285003f2dd259b9a165e22920
IvanPranataGunawan <ivan.gunawan1@student.umn.ac.id>
2019-11-11 16:38:46 +0700

    ivan

--- application/views/navbar/dataAnggota_page.php
+++ application/views/navbar/dataAnggota_page.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link rel="stylesheet" href="<?php echo base_url('assets') ?>/style.css" type="text/css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" type="text/css">
 
     <title>E-Library</title>
 </head>
@@ -19,7 +21,7 @@
     <?php $this->load->view('template/header.php') ?>
     <br>
     <!-- content -->
-    <div class="container">
+    <tr class="container">
       <div>
         <h1>Data Anggota</h1>
       </div>
@@ -28,55 +30,60 @@
         
         
         <form method="post" class="form-inline mt-2 mt-md-0">
-          <input type="text" class="form-control mr-sm-0" name="keyword" placeholder="Nama, NIP atau NRP" id="inputSearch">
-          <button type="submit" name="select" value="select" class="btn btn-dark my-0 my-sm-0">Cari</button>
+          <input type="text" class="form-control mr-sm-0" id="inputSearch" onkeyup="myFunction()" placeholder="Nama, NIP atau NRP" title="Type in a name">
         </form>
       </div>
 
-      <div class="container" id="marketing">
-        <div class="row">
+     <table id="myTable" class="table table-striped table-bordered" style="width:100%; text-align:center;">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Gambar</th>
+                <th>Nama</th>
+                <th>Jabatan</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
             <?php
-                $count  = count($listUser);
-                echo '<script>console.log("ini0")</script>';
-
-                if(isset($_POST['select']) && $_POST['keyword'] != NULL){
-                  for($i = 0; $i < $count; $i++){
-                    if($listUser[$i]["username"] == $_POST['keyword'] || $listUser[$i]["id"] == $_POST['keyword']){
-                    echo '<div class="col-lg-4" id="kolom">';
-                    ?>
-                    <img class="w-50" src="<?php echo base_url('upload/user/' . $listUser[$i]["image"]) ?>"/>
-                    <?php
-                    echo '<title>Placeholder</title>' ;
-                    echo '<rect width="100%" height="100%" fill="#777"></rect>' ;
-                    echo '</svg>' ;
-                        
-                      echo '<br>';
-                      echo '<br>';
-                      echo '<h5>' . $listUser[$i]["id"] . '</h5>';
-                      echo '<p>'. $listUser[$i]["username"] . '</p>';
-                      echo '</div>';
-                    } 
-                  }
-                }
-                else{
-                  for($i = 0; $i < $count; $i++){
-                    echo '<div class="col-lg-4" id="kolom">';
-                    ?>
-                    <img class="w-50" src="<?php echo base_url('upload/user/' . $listUser[$i]["image"]) ?>"/>
-                    <?php
-                    echo '<title>Placeholder</title>' ;
-                    echo '<rect width="100%" height="100%" fill="#777"></rect>' ;
-                    echo '</svg>' ;
-    
-                      echo '<br>';
-                      echo '<br>';
-                      $urutan = $i+1;
-                      echo '<h5>' . $urutan . '</h5>';
-                      echo '<p>Nama : ' . $listUser[$i]["username"] . '</p>';
-                      echo '</div>';
-                  }
+              $count  = count($listUser);
+              for ($i = 0; $i < $count; $i++) {
+                    echo '<tr>';
+                    echo '<td>'. $i .'</td>';
+            ?>
+            <td><img src="<?php echo base_url('upload/user/' . $listUser[$i]["image"]) ?>"/></td>
+            <?php
+                    echo '<td>'. $listUser[$i]["username"] .'</td>';
+                    echo '<td>mantap</td>';
+                    echo '<td>'  . $listUser[$i]["email"]   . '</td>';
+                    echo '</tr>';
                 }
             ?>
+        </tbody>
+    </table>
+
+    <script>
+      $(document).ready(function(){
+        $('#inputSearch').keyup(function(){
+          search_table($(this).val());
+        });
+
+        function search_table(value){
+          $('#myTable tr').each(function(){
+            var found = 'false';
+            if($(this).text().toLowerCase().indexOf(value.toLowerCase()) >= 0){
+              found = 'true';
+            }
+          });
+          if(found == 'true'){
+            $(this).show();
+          } else{
+            $(this).hide();
+          }
+        }
+      });
+    </script>
+
       </div>
     </div>
   </div>
@@ -88,6 +95,8 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
 </body>
 
 <!-- footer -->
