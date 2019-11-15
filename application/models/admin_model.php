111b82968a2a1b1303a0caf10480968c3a6ed083
Pragoardhi <ardhiprago00@gmail.com>
2019-11-15 11:32:39 +0700

    notifikasi

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -3,10 +3,39 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class admin_model extends CI_Model
 {
-    public function checkNotifikasi()
+    public function getJudulBuku($id)
+    {
+        $data = $this->db->query("SELECT * FROM [dbo].[Book] WHERE ID = '$id'");
+        return $data->result_array();
+    }
+    public function getUserNama($id)
+    {
+        $data = $this->db->query("SELECT * FROM [dbo].[User] WHERE id = '$id'");
+        return $data->result_array();
+    }
+
+    public function checkNotifikasiBuku()
+    {
+        $data = $this->db->query("SELECT * FROM [dbo].[Book] WHERE Notifikasi = 1");
+        return $data->result_array();
+    }
+    public function checkNotifikasiUser()
+    {
+        $data = $this->db->query("SELECT * FROM [dbo].[User] WHERE Notifikasi = 1");
+        return $data->result_array();
+    }
+    public function checkNotifikasiTransaksi()
     {
         $data = $this->db->query("SELECT * FROM [dbo].[Peminjaman] WHERE Notifikasi = 1");
         return $data->result_array();
+    }
+    public function insertNotifikasiUser($id)
+    {
+        $this->db->query("UPDATE [dbo].[User] SET Notifikasi = 0 WHERE id = $id");
+    }
+    public function insertNotifikasiBuku($id)
+    {
+        $this->db->query("UPDATE [dbo].[Book] SET Notifikasi = 0 WHERE ID = $id");
     }
     public function insertNotifikasiTransaksi($id)
     {
@@ -35,7 +64,7 @@ class admin_model extends CI_Model
     }
     public function getBuku()
     {
-        $data = $this->db->query("SELECT dbo.Book.ID,dbo.Book.Judul,dbo.Tipe_book.Tipe, dbo.Book.Penulis, dbo.Book.Penerbit, dbo.Book.ISBN, dbo.Book.Harga, dbo.Book.Keterangan, dbo.Book.Image, dbo.Book.Year, dbo.Language_book.bahasa FROM dbo.Book INNER JOIN dbo.Tipe_book ON dbo.Book.id_tipe = dbo.Tipe_book.id INNER JOIN dbo.Language_book ON dbo.Book.id_bahasa = dbo.Language_book.id");
+        $data = $this->db->query("SELECT dbo.Book.ID,dbo.Book.Judul,dbo.Tipe_book.Tipe, dbo.Book.Penulis, dbo.Book.Penerbit, dbo.Book.ISBN, dbo.Book.Harga, dbo.Book.Keterangan, dbo.Book.Image, dbo.Book.Year, dbo.Language_book.bahasa, dbo.Book.Notifikasi FROM dbo.Book INNER JOIN dbo.Tipe_book ON dbo.Book.id_tipe = dbo.Tipe_book.id INNER JOIN dbo.Language_book ON dbo.Book.id_bahasa = dbo.Language_book.id");
         // echo $data->result_array();
         return $data->result_array();
     }
@@ -77,11 +106,13 @@ class admin_model extends CI_Model
     }
     public function addUser($addusername, $addemail, $addstatus, $addpassword, $profile)
     {
-        $this->db->query("INSERT INTO [dbo].[User](email,pass,status,username,image) VALUES ('$addemail','$addpassword','$addstatus','$addusername','$profile')");
+        $notifikasi = 1;
+        $this->db->query("INSERT INTO [dbo].[User](email,pass,status,username,image,Notifikasi) VALUES ('$addemail','$addpassword','$addstatus','$addusername','$profile','$notifikasi')");
     }
     public function addBuku($judul, $tipe, $penulis, $penerbit, $isbn, $harga, $keterangan, $image, $year, $bahasa)
     {
-        $this->db->query("INSERT INTO [dbo].[Book](Judul,id_tipe,Penulis,Penerbit,ISBN,Harga,Keterangan,Image,Year,id_bahasa) VALUES ('$judul','$tipe','$penulis','$penerbit','$isbn','$harga','$keterangan','$image','$year','$bahasa')");
+        $notifikasi = 1;
+        $this->db->query("INSERT INTO [dbo].[Book](Judul,id_tipe,Penulis,Penerbit,ISBN,Harga,Keterangan,Image,Year,id_bahasa,Notifikasi) VALUES ('$judul','$tipe','$penulis','$penerbit','$isbn','$harga','$keterangan','$image','$year','$bahasa','$notifikasi')");
     }
     public function addTipe($tipe)
     {
